f3282dbe3f1fd7101b9eed52bb52cf1c0870986a
Ivan <hladysh.im@gmail.com>
2016-05-10 09:48:28 +0000

    add paralax img

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
-$('#btn-send').click(function(e) {
-	e.preventDefault();
-	$.ajax({
-		url: "https://formspree.io/hladysh.im@gmail.com",
-		method: "POST",
-		data: {
-			name: names.value,
-			email: email.value,
-			message: message.value
-		},
-		dataType: "json"
-	}).done(function() {
-		alert("Thanks, I'll write to you!")
-		$('input').val('');
-		$('textarea').val('');
-	});
-	
-});
-
-$(function() {
-	$('.js-nav a, .js-connect, .navbar-brand').click(function(e) {
+$(document).ready(function() {
+	$('#btn-send').click(function(e) {
 		e.preventDefault();
-		$('body, html').animate({
-			scrollTop: $($.attr(this, 'href')).offset().top
-		}, 750);
+		$.ajax({
+			url: "https://formspree.io/hladysh.im@gmail.com",
+			method: "POST",
+			data: {
+				name: names.value,
+				email: email.value,
+				message: message.value
+			},
+			dataType: "json"
+		}).done(function() {
+			alert("Thanks, I'll write to you!")
+			$('input').val('');
+			$('textarea').val('');
+		});
+
+	});
+
+	$(function() {
+		$('.js-nav a, .js-connect, .navbar-brand').click(function(e) {
+			e.preventDefault();
+			$('body, html').animate({
+				scrollTop: $($.attr(this, 'href')).offset().top
+			}, 750);
+		});
+	});
+
+	$.stellar({
+		responsive: true
 	});
 });
